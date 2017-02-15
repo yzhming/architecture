79a1e6d7a6345a025759914465fe360937c48ac8
yzhming <yzhmingming@163.com>
2017-02-15 17:29:45 +0800

    reconstructor

---
+++ goodsmgr/src/main/java/com/sishuok/architecture1/goodsmgr/Goods1.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @Title:Goods.java
+ * @Description: TODO
+ * @author:Administrator
+ * @date:2017年1月22日
+ * @version V2.0 
+ *
+ */
+package com.sishuok.architecture1.goodsmgr;
+
+
+public class Goods1 {
+	public void g1(){
+		System.out.println("now in goods1===");
+	}
+}
